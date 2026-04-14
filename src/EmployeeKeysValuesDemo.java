bfce1f60ca40ea01dc0d43c0ce5b4afc3ddc2c8b
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-14 16:50:46 +0400

    Task 8: displays the full map, all keys, and all values

---
+++ src/EmployeeKeysValuesDemo.java
@@ -0,0 +1,36 @@
+import java.util.Map;
+import java.util.HashMap;
+public class EmployeeKeysValuesDemo {
+    public static void main(String[] args) {
+        //Declare a HashMap variable
+        HashMap<Integer,String > employees = new HashMap<>();
+
+        //add record to the map
+        employees.put(123,"Ali");
+        employees.put(124,"Reem");
+        employees.put(125,"Huda");
+        employees.put(126,"Nasser");
+        employees.put(127,"Noor");
+        //Display all elements in the HashMap
+        for(Map.Entry<Integer,String > emp : employees.entrySet()){
+            System.out.println("Employee ID: " + emp.getKey() + " Employee Name: " + emp.getValue());
+
+
+        }
+        //Display all keys in the HashMap
+        for (Integer key : employees.keySet()) {
+            System.out.println(key);
+        }
+        //Display all values in the HashMap
+        for (String val : employees.values()) {
+            System.out.println((val));
+        }
+
+        employees.put(128, "Sara");
+        System.out.println(employees);
+
+
+
+
+    }
+}
