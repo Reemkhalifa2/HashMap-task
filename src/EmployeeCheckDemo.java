12865dc1e0b73bc89103d3d4aa80492b20229e35
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-14 16:34:08 +0400

    Task 3:  checks

---
+++ src/EmployeeCheckDemo.java
@@ -0,0 +1,28 @@
+import java.util.Map;
+import java.util.HashMap;
+public class EmployeeCheckDemo {
+    public static void main(String[] args) {
+        //Declare a HashMap variable
+        HashMap<Integer,String > employees = new HashMap<>();
+
+        //add record to the map
+        employees.put(123,"Ali");
+        employees.put(124,"Reem");
+        employees.put(125,"Huda");
+        employees.put(126,"Nasser");
+        employees.put(127,"Noor");
+
+        //Display all elements in the HashMap
+        for(Map.Entry<Integer,String > emp : employees.entrySet()){
+            System.out.println("Employee ID: " + emp.getKey() + " Employee Name: " + emp.getValue());
+
+        }
+
+        //Use containsKey() to check whether a specific employee ID exists
+        System.out.println(employees.containsKey(127));
+        System.out.println(employees.containsKey(135));
+
+
+
+    }
+}
