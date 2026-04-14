59733d6c97da33c1ca6c8d5562a7be6e016f37bc
Reem Khalifa <aladawi12reem6@gmail.com>
2026-04-14 15:54:11 +0400

    Task 1: store and manage Employees record using Hashmap

---
+++ src/EmployeeHashMapDemo.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+import java.util.HashMap;
+public class EmployeeHashMapDemo {
+    public static void main(String[] args) {
+        //Declare a HashMap variable
+        HashMap<Integer,String > employees = new HashMap<>();
+
+        //add record to the map
+        employees.put(123,"Ali");
+        employees.put(124,"Reem");
+        employees.put(125,"Huda");
+        employees.put(126,"Nasser");
+        employees.put(127,"Noor");
+
+        //Display all elements in the HashMap
+        for(Map.Entry<Integer,String > emp : employees.entrySet()){
+            System.out.println("Employee ID: " + emp.getKey() + " Employee Name: " + emp.getValue());
+
+        }
+
+        // Add one more record and display the updated HashMap
+        employees.put(128,"Farah");
+        System.out.println(employees);
+
+
+    }
+}
